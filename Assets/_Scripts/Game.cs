dd200200c3fd94a4cc8de2708b5cdee92b10491d
Tefgoo <tefgoov@gmail.com>
2023-04-01 13:57:47 -0500

    Clicks working, shop still dont
    Added the main button to add money, it works now, shop still wip

---
+++ Assets/_Scripts/Game.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Game : MonoBehaviour
+{
+    //Clicks
+    public TMP_Text scoreText;
+    public float currentScore;
+    public float hitPower;
+    public float scoreIncreasePerSecond;
+    public float x;
+
+    //Shop
+    public int shop1prize;
+    public TMP_Text shop1text;
+
+    public int shop2prize;
+    public TMP_Text shop2text;
+
+    //Amount
+    public TMP_Text amount1text;
+    public int amount1;
+    public float amount1profit;
+
+    public TMP_Text amount2text;
+    public int amount2;
+    public float amount2profit;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Clicks
+        currentScore = 0;
+        hitPower = 1;
+        scoreIncreasePerSecond = 1;
+        x = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Clicks
+        scoreText.text = (int)currentScore + "$";
+        scoreIncreasePerSecond = x * Time.deltaTime;
+        currentScore = currentScore + scoreIncreasePerSecond;
+
+        //Shop
+        shop1text.text = "Tier 1: " + shop1prize + " $";
+        shop2text.text = "Tier 2: " + shop2prize + " $";
+
+        //Amount
+        amount1text.text = "Tier 1: " + amount1 + "arts $: " + amount1profit + "/s";
+        amount2text.text = "Tier 2: " + amount2 + "arts $: " + amount2profit + "/s";
+    }
+
+    //Hit
+    public void Hit()
+    {
+        currentScore += hitPower;
+    }
+}
