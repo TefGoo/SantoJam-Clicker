d43f10e805975c022b3eb28b090b676906f6f0db
Tefgoo <tefgoov@gmail.com>
2023-04-05 17:50:49 -0500

    Working shop
    Now shop is working but still ugly, still missing sprites and real text

--- Assets/_Scripts/Game.cs
+++ Assets/_Scripts/Game.cs
@@ -20,6 +20,15 @@ public class Game : MonoBehaviour
     public int shop2prize;
     public TMP_Text shop2text;
 
+    public int shop3prize;
+    public TMP_Text shop3text;
+
+    public int shop4prize;
+    public TMP_Text shop4text;
+
+    public int shop5prize;
+    public TMP_Text shop5text;
+
     //Amount
     public TMP_Text amount1text;
     public int amount1;
@@ -28,6 +37,18 @@ public class Game : MonoBehaviour
     public TMP_Text amount2text;
     public int amount2;
     public float amount2profit;
+
+    public TMP_Text amount3text;
+    public int amount3;
+    public float amount3profit;
+
+    public TMP_Text amount4text;
+    public int amount4;
+    public float amount4profit;
+
+    public TMP_Text amount5text;
+    public int amount5;
+    public float amount5profit;
 
     //Upgrade
     public int upgradePrize;
@@ -65,10 +86,19 @@ public class Game : MonoBehaviour
         //Default variables
         shop1prize = 25;
         shop2prize = 125;
+        shop3prize = 300;
+        shop4prize = 550;
+        shop5prize = 1000;
         amount1 = 0;
         amount1profit = 1;
         amount2 = 0;
         amount2profit = 5;
+        amount3 = 0;
+        amount3profit = 10;
+        amount4 = 0;
+        amount4profit = 20;
+        amount5 = 0;
+        amount5profit = 40;
 
         //Reset
         //PlayerPrefs.DeleteAll();
@@ -79,10 +109,19 @@ public class Game : MonoBehaviour
         x = PlayerPrefs.GetInt("x", 0);
         shop1prize = PlayerPrefs.GetInt("shop1prize", 25);
         shop2prize = PlayerPrefs.GetInt("shop2prize", 125);
+        shop3prize = PlayerPrefs.GetInt("shop3prize", 300);
+        shop4prize = PlayerPrefs.GetInt("shop4prize", 550);
+        shop5prize = PlayerPrefs.GetInt("shop5prize", 1000);
         amount1 = PlayerPrefs.GetInt("amount1", 0);
         amount1profit = PlayerPrefs.GetInt("amount1profit", 0);
         amount2 = PlayerPrefs.GetInt("amount2", 0);
         amount2profit = PlayerPrefs.GetInt("amount2profit", 0);
+        amount3 = PlayerPrefs.GetInt("amount3", 0);
+        amount3profit = PlayerPrefs.GetInt("amount3profit", 0);
+        amount4 = PlayerPrefs.GetInt("amount4", 0);
+        amount4profit = PlayerPrefs.GetInt("amount4profit", 0);
+        amount5 = PlayerPrefs.GetInt("amount5", 0);
+        amount5profit = PlayerPrefs.GetInt("amount5profit", 0);
         upgradePrize = PlayerPrefs.GetInt("upgradePrize", 50);
 
         //New
@@ -101,10 +140,16 @@ public class Game : MonoBehaviour
         //Shop
         shop1text.text = "Tier 1: " + shop1prize + " $";
         shop2text.text = "Tier 2: " + shop2prize + " $";
+        shop3text.text = "Tier 3: " + shop3prize + " $";
+        shop4text.text = "Tier 4: " + shop4prize + " $";
+        shop5text.text = "Tier 5: " + shop5prize + " $";
 
         //Amount
         amount1text.text = "Tier 1: " + amount1 + "arts $: " + amount1profit + "/s";
         amount2text.text = "Tier 2: " + amount2 + "arts $: " + amount2profit + "/s";
+        amount3text.text = "Tier 3: " + amount3 + "arts $: " + amount3profit + "/s";
+        amount4text.text = "Tier 4: " + amount4 + "arts $: " + amount4profit + "/s";
+        amount5text.text = "Tier 5: " + amount5 + "arts $: " + amount5profit + "/s";
 
         //Upgrade
         UpgradeText.text = "Cost: " + upgradePrize + " $";
@@ -115,14 +160,23 @@ public class Game : MonoBehaviour
         PlayerPrefs.SetInt("x", (int)x);
         PlayerPrefs.SetInt("shop1prize", (int)shop1prize);
         PlayerPrefs.SetInt("shop2prize", (int)shop2prize);
+        PlayerPrefs.SetInt("shop3prize", (int)shop3prize);
+        PlayerPrefs.SetInt("shop4prize", (int)shop4prize);
+        PlayerPrefs.SetInt("shop5prize", (int)shop5prize);
         PlayerPrefs.SetInt("amount1", (int)amount1);
         PlayerPrefs.SetInt("amount1profit", (int)amount1profit);
         PlayerPrefs.SetInt("amount2", (int)amount2);
         PlayerPrefs.SetInt("amount2profit", (int)amount2profit);
+        PlayerPrefs.SetInt("amount3", (int)amount3);
+        PlayerPrefs.SetInt("amount3profit", (int)amount3profit);
+        PlayerPrefs.SetInt("amount4", (int)amount1);
+        PlayerPrefs.SetInt("amount4profit", (int)amount4profit);
+        PlayerPrefs.SetInt("amount5", (int)amount5);
+        PlayerPrefs.SetInt("amount5profit", (int)amount5profit);
         PlayerPrefs.SetInt("upgradePrize", (int)upgradePrize);
 
         //Achievement
-        if (currentScore >= 50)
+        if (currentScore >= 1000)
         {
             achievementScore = true;
         }
@@ -213,6 +267,42 @@ public class Game : MonoBehaviour
             amount2profit += 5;
             x += 5;
             shop2prize += 125;
+        }
+    }
+
+    public void Shop3()
+    {
+        if (currentScore >= shop3prize)
+        {
+            currentScore -= shop3prize;
+            amount3 += 1;
+            amount3profit += 10;
+            x += 10;
+            shop3prize += 300;
+        }
+    }
+
+    public void Shop4()
+    {
+        if (currentScore >= shop4prize)
+        {
+            currentScore -= shop4prize;
+            amount4 += 1;
+            amount4profit += 20;
+            x += 20;
+            shop4prize += 550;
+        }
+    }
+
+    public void Shop5()
+    {
+        if (currentScore >= shop5prize)
+        {
+            currentScore -= shop5prize;
+            amount5 += 1;
+            amount5profit += 40;
+            x += 40;
+            shop5prize += 1000;
         }
     }
 
